98088d57ed4edad894535451a4f7940105b57d90
Gaevoy <gaevoyrs@gmail.com>
2023-11-17 00:37:57 +0300

    delete service param

--- internal/infrastructure/di/wire.go
+++ internal/infrastructure/di/wire.go
@@ -6,21 +6,13 @@ package di
 //go:generate wire
 
 import (
-	"github.com/3110Y/profile/internal/application/service"
-	"github.com/3110Y/profile/internal/application/validator"
 	"github.com/3110Y/profile/internal/infrastructure/database"
-	"github.com/3110Y/profile/internal/infrastructure/repository"
-	"github.com/3110Y/profile/internal/presentation/rpc"
 	"github.com/google/wire"
 	"github.com/jmoiron/sqlx"
 )
 
 type DI struct {
-	ProfileService    *service.ProfileService
-	ProfileValidator  *validator.ProfileValidator
-	ProfileRepository *repository.ProfileRepository
-	ProfileRPC        *rpc.ProfileRPC
-	DB                *sqlx.DB
+	DB *sqlx.DB
 }
 
 func NewDI(
